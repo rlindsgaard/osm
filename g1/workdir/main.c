4546f52e6997b1e9d3a6a06b08064bcdf804ba38
Ronni Elken Lindsgaard <ronni.lindsgaard@gmail.com>
2010-02-10 13:08:18 +0100

    moved workdir correctly

--- g1/workdir/main.c
+++ g1/workdir/main.c
@@ -13,27 +13,20 @@ int pqueue_t()
  int five = 5;
  int six = 6;
 
- printf("Removing from an empty queue. Result: %d\n",pqueue_remove(pq));
- printf("Queueing nothing. Result: %d\n",pqueue_insert(NULL,1,&one));
- printf("Inserting null element. Result: %d\n",pqueue_insert(pq,1,NULL));
- 
- //building queue
  pqueue_insert(pq,1,&one);
  pqueue_insert(pq,2,&two);
  pqueue_insert(pq,3,&three);
  pqueue_insert(pq,3,&four);
  pqueue_insert(pq,5,&five);
  pqueue_print(pq);
- 
- //checking fifo
+
+ printf("We are now ready to make errors\n");
+
  pqueue_insert(pq,3,&six);
  pqueue_print(pq);
-
- //removing in the right order
  pqueue_remove(pq);
- pqueue_print(pq);
  pqueue_remove(pq);
+ pqueue_insert(pq,1,&one);
  pqueue_print(pq);
-
 
 }
