88d05bf6f51f1b042edb5034f6bc05cfe8a074a8
Ronni Elken Lindsgaard <ronni.lindsgaard@gmail.com>
2010-03-03 10:54:25 +0100

    Added the pqueue from g1, maybe they need to be threadsafe

---
+++ example/source/pqueue.c
@@ -0,0 +1,96 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "pqueue.h"
+#include "wqueue.h"
+struct entry {
+  int pri;
+  int age;
+  entry_t * next;
+  void *elem;
+};
+
+int pqueue_insert(pqueue_t *pq, unsigned int pri, void *elem)
+{
+  entry_t *previous;
+  entry_t *current;
+  if(pq->counter > 0 && pq->counter % 20 == 0)
+    pqueue_starve(pq);
+  pq->counter++;
+  current = pq->e;
+  //Create new entry
+  entry_t *new_elem;
+  new_elem = calloc(1,sizeof(entry_t));
+  if(new_elem == NULL)  //assert that we get allocated memory
+    return -1; 
+  new_elem->pri = pri;
+  new_elem->elem = elem;
+  new_elem->next = NULL;
+  new_elem->age = pq->counter;
+
+  previous = NULL;
+  //Run through the sorted list
+  while(current != NULL && pri >= current->pri)
+  {
+
+    previous = current;
+    current = current->next;
+  }
+  new_elem->next = current;
+  
+  if(previous != NULL)
+  {
+    previous->next = new_elem;
+  } else {
+    pq->e = new_elem;
+  }
+  return 0;
+}
+
+void *pqueue_remove(pqueue_t *pq)
+{
+  /*
+   * 1. Copy the entry value
+   * 2. Update the pqueue
+   * 3. Free memory from the late first entry
+   * 4. Return the pointer to the element
+   * 5. Profit
+   */
+
+  if(pq->e == NULL)
+    return NULL;
+  void *t_elem;
+  entry_t *t_entry;
+  t_entry = pq->e;
+  
+  
+  t_elem = t_entry->elem;
+  pq->e = t_entry->next;
+
+  free(t_entry);
+  return t_elem;
+}
+
+void pqueue_starve(pqueue_t *pq)
+{
+  entry_t *current;
+  current = pq->e;
+  entry_t *last_pri;
+  entry_t *previous;
+
+  while(current != NULL)
+  {
+    if(current->pri > previous->pri)
+      last_pri = previous;
+    if(pq->counter - current->age > 20)
+    {
+      current->pri++;
+      current->age = pq->counter;
+      previous->next = current->next;
+      current->next = last_pri->next;
+      last_pri->next = current;
+    }
+    previous = current;
+    current = current->next;
+  }
+}
+
---
+++ example/source/pqueue.h
@@ -0,0 +1,38 @@
+#ifndef PQUEUE_GUARD
+#define PQUEUE_GUARD 1
+#define PQUEUE_INIT {NULL}
+
+/*
+Åben definition af en prioriteteskø instans. Denne kan
+ændres efter behov, sålænge det ikke ændrer på nedenstående
+funktioners grænseflade.
+*/
+typedef struct pqueue pqueue_t;
+
+typedef struct entry entry_t;
+
+struct pqueue {
+    int counter;
+    entry_t *e;
+};
+
+/*
+Indsætter et elementet som 'elem' peger på, med prioriteten 'pri'.
+Det er muligt at indsætte flere elementer med samme prioritet.
+'pq' peger på en instans af prioritetskøen.
+
+Lykkedes indsættelsen skal funktionen returnerer '0'. Ved fejl '-1'.
+*/
+int pqueue_insert(pqueue_t *pq, unsigned int pri, void *elem);
+
+
+/*
+Returnerer det element som har højst prioritet. Hvis flere elementer
+har samme højeste prioritet returneres det af dem, som først blev indsat.
+
+Hvis køen er tom, returneres 'NULL'.
+*/
+void *pqueue_remove(pqueue_t *pq);
+
+void pqueue_starve(pqueue_t *pq);
+#endif //PQUEUE_GUARD
